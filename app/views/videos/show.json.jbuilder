9a9056ca6114697eaefc0c64fd7cec440881d7aa
Silverhawk <sondra.silverhawk@hush.com>
2017-01-24 09:08:39 -0800

    Add scaffold for videos

---
+++ app/views/videos/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @video, :id, :title, :url, :thumbnail, :id, :description, :tags, :created_at, :updated_at
---
+++ db/migrate/20170124170010_create_videos.rb
@@ -0,0 +1,14 @@
+class CreateVideos < ActiveRecord::Migration
+  def change
+    create_table :videos do |t|
+      t.string :title
+      t.string :url
+      t.string :thumbnail
+      t.string :id
+      t.string :description
+      t.string :tags
+
+      t.timestamps null: false
+    end
+  end
+end
